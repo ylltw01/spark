ba73fac13c4bd9cb988fc7208800d706196ea675
noone <ylltw01@163.com>
2020-12-31 16:35:59 +0800

    spark.streaming.concurrentJobs comment

--- streaming/src/main/scala/org/apache/spark/streaming/scheduler/JobScheduler.scala
+++ streaming/src/main/scala/org/apache/spark/streaming/scheduler/JobScheduler.scala
@@ -49,6 +49,7 @@ class JobScheduler(val ssc: StreamingContext) extends Logging {
   // Use of ConcurrentHashMap.keySet later causes an odd runtime problem due to Java 7/8 diff
   // https://gist.github.com/AlainODea/1375759b8720a3f9f094
   private val jobSets: java.util.Map[Time, JobSet] = new ConcurrentHashMap[Time, JobSet]
+  // 控制 sparkStreaming 并行执行job 个数
   private val numConcurrentJobs = ssc.conf.getInt("spark.streaming.concurrentJobs", 1)
   private val jobExecutor =
     ThreadUtils.newDaemonFixedThreadPool(numConcurrentJobs, "streaming-job-executor")
